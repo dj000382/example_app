250afd15a1f29eb55bdfbad9fd3c9f3ba1093bc9
Durando <dj000382@etu.unice.fr>
2012-11-07 14:17:24 +0100

    amelio code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_titre)
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_titre.empty?
+      return base_title
+    else
+      return "#{base_title} | #{page_titre}"
+    end
+  end
 end
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe "Static pages" do
+  describe "Home page" do
+    it "should have the content 'Example App'" do
+      visit '/static_pages/home'
+      page.should have_content('Example App')
+    end
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',:text => "Ruby on Rails Tutorial Example App | Home")
+    end
+  end
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_content('Help')
+    end
+  end
+  describe "About page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/about'
+      page.should have_content('About')
+    end
+  end
+end
