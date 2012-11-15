f9bf3814acb1fdfb92d78e81e65388e1ecf00689
Durando <dj000382@etu.unice.fr>
2012-11-15 13:09:56 +0100

    Presque fini tp7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,12 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
   end
+	def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
+ 	end
 end
