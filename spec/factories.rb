033ab0856a00f32f677563604cda04e228f9ea6a
Durando <jimmydurando06@gmail.com>
2012-11-30 11:01:10 +0100

    tout fini

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,9 @@
 FactoryGirl.define do
-	factory :user do
-		name "Jimmy"
-		email "foobar@example.com"
-		password "coucou"
-		password_confirmation "coucou"
-	end
+  factory :user do
+    name     "Michael Hartl"
+    email    "michael@example.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
 end
+
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,28 +1,37 @@
 require 'spec_helper'
+require 'application_helper'
 
 describe "Static pages" do
+
+  subject { page }
+
   describe "Home page" do
-    it "should have the content 'Example App'" do
-      visit root_path
-      page.should have_content('Example App')
-    end
-    it "should have the right title" do
-      visit root_path
-      page.should have_selector('title',:text => "Ruby on Rails Tutorial Sample App")
-    end
+    before { visit root_path }
+
+    it { should have_selector('h1',    text: 'Example App') }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector 'title', text: '| Home' }
   end
+
   describe "Help page" do
-    it "should have the content 'Help'" do
-      visit help_path
-      page.should have_content('Help')
-    end
+    before { visit help_path }
+
+    it { should have_selector('h1',    text: 'Help') }
+    it { should have_selector('title', text: full_title('Help')) }
   end
+
   describe "About page" do
-    it "should have the content 'Help'" do
-      visit about_path
-      page.should have_content('About')
-    end
+    before { visit about_path }
+
+    it { should have_selector('h1',    text: 'About') }
+    it { should have_selector('title', text: full_title('About Us')) }
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+
+    it { should have_selector('h1',    text: 'Contact') }
+    it { should have_selector('title', text: full_title('Contact')) }
   end
 end
-
 
