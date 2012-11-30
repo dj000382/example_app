686e352326ea8c4cade95b1b0730de349d58137e
Durando <jimmydurando06@gmail.com>
2012-11-30 09:25:28 +0100

    avancé chap8 encore

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,9 +13,15 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password_digest , :password, :password_confirmation
   has_secure_password
 	before_save { |user| user.email = email.downcase}
+	before_save :create_remember_token
   validates :name, presence: true, length: {maximum: 50}
 	validates :password, presence: true, length: {minimum: 6}
 	validates :password_confirmation, presence: true
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true , uniqueness: {case_sensitive: false}, format: { with: VALID_EMAIL_REGEX }
+private
+	def create_remember_token
+		# create the token
+		self.remember_token = SecureRandom.urlsafe_base64
+	end
 end
